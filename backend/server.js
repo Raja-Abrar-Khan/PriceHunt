3e32b85b289e27cfe729f9b8c281ede31b21f018
Raja Abrar Khan <abrarkhan778008@gmail.com>
2025-02-05 12:41:16 +0530

    sever made

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import colors from "colors";
+import { PrismaClient } from "@prisma/client";
+import { config } from "dotenv";
+import cors from "cors";
+
+config();
+
+const prisma =new PrismaClient();
+const app=express();
+const PORT=process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("hieeee");
+})
+const connectPostgress=async()=>{
+try {
+    const result=await prisma.$queryRaw`SELECT current_database()`;
+    console.log(`connected to the database: ${result[0].current_database}`.bgWhite.black);
+} catch (error) {
+    console.error("Error connection to the datatbase".bgRed.white,error.message);
+}
+}
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`.bgGreen.bgBlack);
+    connectPostgress();
+})
